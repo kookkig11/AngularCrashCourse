3f9dac1f2aee756287d3d5daef30f7d3e594d02a
intrusive <kanittha.wo@ku.th>
2020-10-28 13:45:57 +0700

    ✨ create checkbox with primitive type array

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -7,13 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todo List';
-  todos = ['Item 1', 'Item 2', 'Item 3'];
+  todos : Todo[] = [
+    {
+      userId : 1,
+      id : 1,
+      title : 'Todo 1',
+      completed : true
+    },
+    {
+      userId : 2,
+      id : 2,
+      title : 'Todo 2',
+      completed : false
+    },
+    {
+      userId : 3,
+      id : 3,
+      title : 'Todo 3',
+      completed : false
+    }
+  ];
+  
   count = 0;
-
   clickLike() : void {
     this.count++;
   }
   clickUnLike() : void {
     this.count--;
   }
+}
+
+interface Todo {
+  userId : number;
+  id : number;
+  title : string;
+  completed : boolean;
 }
