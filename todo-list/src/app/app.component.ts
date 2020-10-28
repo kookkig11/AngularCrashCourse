8d341d0c12637b0ab02a50c08c5c49eac639d9c9
intrusive <kanittha.wo@ku.th>
2020-10-28 11:32:05 +0700

    ✨ test sample coding

--- todo-list/src/app/app.component.ts
+++ todo-list/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'Todo List';
+  todos = ['Item 1', 'Item 2', 'Item 3'];
+  count = 0;
+
+  clickLike() : void {
+    this.count++;
+  }
+  clickUnLike() : void {
+    this.count--;
+  }
 }
